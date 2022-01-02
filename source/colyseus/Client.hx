15ae17067380062f1b7c8fead6d915ccdee56e31
melonin <hazagames404@gmail.com>
2022-01-02 20:42:15 +0700

    Move colyseus.Room to colyseus.Client.Room

--- source/colyseus/Client.hx
+++ source/colyseus/Client.hx
@@ -1,9 +1,21 @@
 package colyseus;
 
+import haxe.extern.EitherType;
+
 @:luaRequire("colyseus.client")
 extern class Client {
     
     @:luaDotMethod public function new(endpoint:String);
     public function join_or_create(room_name:String, options:Dynamic, callback:(error:String, room:Room)->Void):Void;
+
+}
+
+extern class Room {
+    
+    public var state(default, never):Dynamic<{}>;
+    public var id(default, never):String;
+    public var name(default, never):String;
+
+    public function send(type:EitherType<String, Int>, message:Dynamic):Void;
 
 }
