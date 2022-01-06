a85ac6384cc6d96267d50c2c87e56dc39c154cc2
melonin <hazagames404@gmail.com>
2022-01-06 19:32:17 +0700

    Update Room & add RoomEvent

--- source/colyseus/Client.hx
+++ source/colyseus/Client.hx
@@ -1,5 +1,6 @@
 package colyseus;
 
+import haxe.Constraints.Function;
 import haxe.extern.EitherType;
 
 @:luaRequire("colyseus.client")
@@ -12,10 +13,17 @@ extern class Client {
 
 extern class Room {
     
-    public var state(default, never):Dynamic<{}>;
+    public var state(default, never):Dynamic;
     public var id(default, never):String;
     public var name(default, never):String;
 
-    public function send(type:EitherType<String, Int>, message:Dynamic):Void;
+    public function send(type:EitherType<String, Int>, ?message:Dynamic):Void;
+    public function on(event:RoomEvent, handler:Function):Void;
 
+}
+
+enum abstract RoomEvent(String) {
+
+    var STATE_CHANGE = "statechange";
+    
 }
