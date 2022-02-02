51d122eab23259aec827c201444c2c4f39e88366
melonin <hazagames404@gmail.com>
2022-02-02 16:40:01 +0700

    Add Room 'sessionId'

--- source/colyseus/Client.hx
+++ source/colyseus/Client.hx
@@ -14,6 +14,7 @@ extern class Client {
 extern class Room {
     
     public var state(default, never):Dynamic;
+    public var sessionId(default, never):String;
     public var id(default, never):String;
     public var name(default, never):String;
 
