ffb613c97f77253963d6f5a39ce1e07405958897
melonin <hazagames404@gmail.com>
2022-02-16 19:07:53 +0700

    Add Client create()

--- source/colyseus/Client.hx
+++ source/colyseus/Client.hx
@@ -8,6 +8,7 @@ extern class Client {
     
     @:luaDotMethod public function new(endpoint:String);
     public function join_or_create(room_name:String, options:Dynamic, callback:(error:String, room:Room)->Void):Void;
+    public function create(room_name:String, options:Dynamic, callback:(error:String, room:Room)->Void):Void;
 
 }
 
