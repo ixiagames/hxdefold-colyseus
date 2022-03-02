bdfd7463023c8981eaac3ce2ce31d8d583db4123
melonin <hazagames404@gmail.com>
2022-03-03 02:15:12 +0700

    Add Client join_by_id

--- source/colyseus/Client.hx
+++ source/colyseus/Client.hx
@@ -7,8 +7,9 @@ import haxe.extern.EitherType;
 extern class Client {
     
     @:luaDotMethod public function new(endpoint:String);
-    public function join_or_create(room_name:String, options:Dynamic, callback:(error:String, room:Room)->Void):Void;
     public function create(room_name:String, options:Dynamic, callback:(error:String, room:Room)->Void):Void;
+    public function join_or_create(room_name:String, options:Dynamic, callback:(error:String, room:Room)->Void):Void;
+    public function join_by_id(room_id:String, options:Dynamic, callback:(error:String, room:Room)->Void):Void;
 
 }
 
